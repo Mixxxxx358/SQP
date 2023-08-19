e0ce248f96b609ba15738e9062d3dc44e7ae39a5
Mixxxxx358 <j.h.hoekstra@student.tue.nl>
2023-08-19 13:33:34 +0200

    update to angle model

--- closed_loop.py
+++ closed_loop.py
@@ -6,6 +6,7 @@ import scipy.linalg
 from model import SinCos_encoder
 import deepSI
 from systems import UnbalancedDisc, ReversedUnbalancedDisc
+import time
 
 def main():
     # create ocp object to formulate the OCP
@@ -20,11 +21,13 @@ def main():
     # norm = ss_enc.norm
     norm = normalizer(np.array([0.0,0.0]), np.array([1.0,1.0]), 0, 1)
 
-    nx = 2#model.x.size()[0]
-    nu = 1#model.u.size()[0]
+    # nx = model.x.size()[0]
+    # nu = model.u.size()[0]
+    nx = 2
+    nu = 1
     ny = nx + nu
     ny_e = nx
-    N_horizon = 15
+    N_horizon = 5
     dt = 0.1
     Tf = dt*N_horizon
 
@@ -35,8 +38,8 @@ def main():
     ocp.cost.cost_type = 'NONLINEAR_LS'
     ocp.cost.cost_type_e = 'NONLINEAR_LS'
 
-    Q_mat = 2*np.diag([1e2, 1e2, 1e-2])
-    R_mat = 2*np.diag([1e-1])
+    Q_mat = 2*np.diag([1e2, 1e0])
+    R_mat = 2*np.diag([1e-2])
 
     ocp.cost.W = Q_mat#scipy.linalg.block_diag(Q_mat, R_mat)
 
@@ -52,12 +55,12 @@ def main():
     # ocp.cost.Vx_e = np.eye(nx)
 
     theta_ref = 0
-    ocp.cost.yref  = np.hstack((norm_output((np.sin(theta_ref), np.cos(theta_ref)), norm),0))#np.zeros((ny, ))
-    ocp.cost.yref_e = np.hstack((norm_output((np.sin(theta_ref), np.cos(theta_ref)), norm),0))#np.zeros((ny_e, ))
+    ocp.cost.yref  = np.hstack((norm_output((theta_ref,0), norm)))#np.zeros((ny, ))
+    ocp.cost.yref_e = np.hstack((norm_output((theta_ref,0), norm)))#np.zeros((ny_e, ))
 
 
     # set constraints
-    Fmax = 2.5
+    Fmax = 4.0
     x0 = norm_output(np.array([0.0, 0.0]), norm)
     ocp.constraints.constr_type = 'BGH'
     ocp.constraints.lbu = norm_input(np.array([-Fmax]), norm)
@@ -68,10 +71,11 @@ def main():
     ocp.constraints.idxbu = np.array([0])
     # ocp.constraints.idxbx = np.array([0,0]) # look at this later
 
-    ocp.solver_options.qp_solver = 'PARTIAL_CONDENSING_HPIPM' # FULL_CONDENSING_QPOASES
+    # ocp.solver_options.qp_solver = 'PARTIAL_CONDENSING_HPIPM'
+    ocp.solver_options.qp_solver = 'FULL_CONDENSING_QPOASES'
     ocp.solver_options.hessian_approx = 'GAUSS_NEWTON'
     ocp.solver_options.integrator_type = 'ERK'
-    ocp.solver_options.nlp_solver_type = 'SQP' # SQP_RTI
+    ocp.solver_options.nlp_solver_type = 'SQP_RTI' # SQP_RTI
 
     ocp.solver_options.qp_solver_cond_N = N_horizon
 
@@ -82,30 +86,45 @@ def main():
     acados_ocp_solver = AcadosOcpSolver(ocp, json_file = solver_json)
 
     # create an integrator with the same settings as used in the OCP solver.
-    acados_integrator = AcadosSimSolver(ocp, json_file = solver_json)
+    # acados_integrator = AcadosSimSolver(ocp, json_file = solver_json)
 
-    system = UnbalancedDisc(dt=dt, sigma_n=[0.014])
+    system = UnbalancedDisc(dt=dt, sigma_n=[0.0])
     system.reset_state()
 
-    Nsim = 50
-    # reference = randomLevelReference(Nsim+N_horizon, [10,15], [-np.pi,np.pi])
-    reference = np.hstack((np.ones(200)*2.8))
+    Nsim = 100
+    a = 1.1; reference = np.hstack((np.ones(20)*a,np.ones(20)*-a,np.ones(20)*a/2,np.ones(20)*-a/2,np.ones(40)*0))
+    # reference = np.load("references/multisine.npy")
+    # a=3.1; reference = []
+    # for i in range(40):
+    #     reference = np.hstack((reference, np.ones(5)*a/40*i))
+    # reference = np.hstack((reference, np.ones(40)*a))
 
     simX = np.ndarray((Nsim+1, nx))
     simU = np.ndarray((Nsim, nu))
 
     simX[0,:] = denorm_output(x0, norm)
     total_time = 0
+
+    comp_time_total = np.zeros(Nsim)
+    solver_time_total = np.zeros(Nsim)
+    lin_time_total = np.zeros(Nsim)
+
+    start_time = time.time()
     
     # closed loop
     for i in range(Nsim):
         for j in range(1,N_horizon):
-            acados_ocp_solver.cost_set(j, 'yref', np.hstack((norm_output((np.sin(reference[i+j]), np.cos(reference[i+j])), norm),0)))
-        acados_ocp_solver.cost_set(N_horizon, 'yref', np.hstack((norm_output((np.sin(reference[i+N_horizon]), np.cos(reference[i+N_horizon])), norm),0)))
+            acados_ocp_solver.cost_set(j, 'yref', np.hstack((norm_output((reference[i+j],0), norm))))
+        acados_ocp_solver.cost_set(N_horizon, 'yref', np.hstack((norm_output((reference[i+N_horizon],0), norm))))
 
         # solve ocp and get next control input
         simU[i,:] = denorm_input(acados_ocp_solver.solve_for_x0(x0_bar = norm_output(simX[i, :], norm)), norm)
+        # acados_ocp_solver.print_statistics()
         total_time += acados_ocp_solver.get_stats('time_tot')
+        comp_time_total[i] = acados_ocp_solver.get_stats('time_tot')
+        solver_time_total[i] = acados_ocp_solver.get_stats('time_qp_solver_call')
+        lin_time_total[i] = acados_ocp_solver.get_stats('time_lin')
+        # print(acados_ocp_solver.get_stats('sqp_iter'))
 
         system.x = system.f(system.x, simU[i,0])
         x1 = system.h(system.x, simU[i,0])
@@ -117,11 +136,23 @@ def main():
         # noise = np.random.normal(0, 0.03, size=(2,))
         # simX[i+1, :] = acados_integrator.simulate(x=simX[i, :], u=simU[i,:]) + noise
 
+    end_time = time.time()
+    print("Sim time: " + str(end_time - start_time) + " [s]")
+
     # plot results
-    simX[:, 1] = (simX[:, 1]+np.pi)%(2*np.pi) - np.pi
+    # simX[:, 1] = (simX[:, 1]+np.pi)%(2*np.pi) - np.pi
     reference = (reference+np.pi)%(2*np.pi) - np.pi
-    print(total_time)
+    print("Total time: " + str(total_time[0]) + " [s]")
+    print("Average time per step: " + str(total_time[0]/Nsim * 1000) + " [ms]")
     plot_pendulum(np.linspace(0, Tf/N_horizon*Nsim, Nsim+1), Fmax, simU, simX, reference)
+
+    # np.save("experiments/ud_sqp_levels_u", simU)
+    # np.save("experiments/ud_sqp_levels_q", simX)
+
+    # np.save("experiments/ud_sqp_sinus_u", simU)
+    # np.save("experiments/ud_sqp_sinus_q", simX)
+
+    print([np.mean(comp_time_total)*1000, np.max(comp_time_total)*1000, np.std(comp_time_total)*1000, np.mean(solver_time_total)*1000, np.mean(lin_time_total)*1000])
 
 
 if __name__ == '__main__':
